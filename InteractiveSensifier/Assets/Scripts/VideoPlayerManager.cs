5ace08ed2598c94caa2ba429ef86158b4704ddaa
LarsHulsmans <lars.hulsmans14@gmail.com>
2020-10-29 17:02:51 +0100

    added a time manager

---
+++ InteractiveSensifier/Assets/Scripts/VideoPlayerManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RenderHeads.Media.AVProVideo;
+
+public class VideoPlayerManager : MonoBehaviour
+{
+    public static VideoPlayerManager instance;
+
+    public MediaPlayer mainPlayer;
+
+    public float totalVideoLenght;
+    public float curentVideoProgress;
+
+    public bool isPlaying;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Setup()
+    {
+
+    }
+
+    private void Update()
+    {
+        if(isPlaying)
+        {
+            curentVideoProgress += Time.deltaTime;
+        }
+    }    
+
+
+
+}
